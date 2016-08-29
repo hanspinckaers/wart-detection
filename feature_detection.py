0f20d9443c145fe96c08060259d43746051c4f68
Hans Pinckaers <hans.pinckaers@gmail.com>
2016-08-29 15:27:20 +0200

    Change to make warts vs negatives tsne

--- feature_detection.py
+++ feature_detection.py
@@ -13,7 +13,7 @@ from kmajority import kmajority, compute_hamming_hist
 from detectors_descriptors import get_descriptor, norm_for_descriptor, get_features_array
 
 
-def analyze_images(detector_name, descriptor_name, n_features, sensitivity, bow_size, gray_detector=True, gray_descriptor=True, cache=False, testing=False):
+def analyze_images(class_one, class_two, detector_name, descriptor_name, n_features, sensitivity, bow_size, gray_detector=True, gray_descriptor=True, cache=False, testing=False, img_size=50, border=5, scatter_size=8000):
     overall_start_time = time.time()
 
     norm = norm_for_descriptor(descriptor_name)
@@ -37,25 +37,6 @@ def analyze_images(detector_name, descriptor_name, n_features, sensitivity, bow_
     # sift-tsne-10-features-per-img.png
     # params: sift = cv2.xfeatures2d.SIFT_create(nfeatures=10, contrastThreshold=0.02, edgeThreshold=2, sigma=0.4)
     # same params for matching
-
-    warts = []
-
-    for root, dirnames, filenames in os.walk("classified/warts"):
-        for filename in fnmatch.filter(filenames, '*.png'):
-            warts.append("classified/warts" + "/" + filename)
-
-    negatives = []
-
-    for root, dirnames, filenames in os.walk("classified/negatives"):
-        for filename in fnmatch.filter(filenames, '*.png'):
-            negatives.append("classified/negatives" + "/" + filename)
-
-    warts_cream = []
-
-    for root, dirnames, filenames in os.walk("classified/warts_cream"):
-        for filename in fnmatch.filter(filenames, '*.png'):
-            warts_cream.append("classified/warts_cream" + "/" + filename)
-
     feature_cache = False
     if os.path.isfile("cache/wart_features" + arg_string + ".npy") and os.path.isfile("cache/warts_cream_features" + arg_string + ".npy"):
         feature_cache = True
@@ -64,13 +45,13 @@ def analyze_images(detector_name, descriptor_name, n_features, sensitivity, bow_
     if not feature_cache or cache or True:
         print str(os.getpid()) + "--- Create features ---"
 
-        wart_features_per_img = get_features_array(warts, sensitivity=sensitivity, detector=detector_name, max_features=n_features, descriptor=descriptor_name, gray_detector=gray_detector, gray_descriptor=gray_descriptor, testing=testing)
+        wart_features_per_img = get_features_array(class_one, sensitivity=sensitivity, detector=detector_name, max_features=n_features, descriptor=descriptor_name, gray_detector=gray_detector, gray_descriptor=gray_descriptor, testing=testing)
         wart_features = [item for sublist in wart_features_per_img for item in sublist]
         wart_features = np.asarray(wart_features)
         if cache:
             np.save("cache/wart_features" + arg_string, wart_features)
 
-        wart_features_cream_per_img = get_features_array(warts_cream, sensitivity=sensitivity, detector=detector_name, max_features=n_features, descriptor=descriptor_name, testing=testing)
+        wart_features_cream_per_img = get_features_array(class_two, sensitivity=sensitivity, detector=detector_name, max_features=n_features, descriptor=descriptor_name, testing=testing)
         wart_features_cream = [item for sublist in wart_features_cream_per_img for item in sublist]
         wart_features_cream = np.asarray(wart_features_cream)
         if cache:
@@ -111,15 +92,6 @@ def analyze_images(detector_name, descriptor_name, n_features, sensitivity, bow_
         if norm != cv2.NORM_L2:
             vocabulary = vocabulary.astype(int)
 
-    # Flann matcher of openCV is quite buggy
-    # So we can only use the BF matches when using Eaclidean distance
-    # if norm == cv2.NORM_L2:
-    #    matcher = cv2.BFMatcher()
-
-    # sift = cv2.xfeatures2d.SIFT_create(nfeatures=10, contrastThreshold=0.02, edgeThreshold=5, sigma=0.4)
-    # extractor = cv2.BOWImgDescriptorExtractor(sift, matcher)
-    # extractor.setVocabulary(vocabulary)
-
     hist_cache = False
     if os.path.isfile("cache/images" + arg_string + ".npy") and os.path.isfile("cache/labels" + arg_string + ".npy") and os.path.isfile("cache/tsne" + arg_string + ".npy"):
         hist_cache = True
@@ -128,9 +100,6 @@ def analyze_images(detector_name, descriptor_name, n_features, sensitivity, bow_
         print str(os.getpid()) + "--- Create histograms ---"
 
         descriptor, _ = get_descriptor(descriptor_name, sensitivity, n_features)
-
-        # if norm == cv2.NORM_L2:
-        #    matcher.add(vocabulary)
 
         histograms = np.zeros((len(warts) + len(warts_cream), bow_size), dtype=np.float32)
         labels = np.zeros(len(warts) + len(warts_cream))
@@ -148,8 +117,6 @@ def analyze_images(detector_name, descriptor_name, n_features, sensitivity, bow_
                 else:
                     img = cv2.imread(warts_cream[j])
 
-                # img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
                 if norm == cv2.NORM_L2:
                     hist = np.zeros(len(vocabulary))
 
@@ -160,12 +127,6 @@ def analyze_images(detector_name, descriptor_name, n_features, sensitivity, bow_
                     hist /= len(descs)
                     hist = hist.astype(np.float32)
 
-                    # kp, _ = sift.detectAndCompute(img_gray, None)
-                    # hist_cv2 = extractor.compute(img, kp)
-
-                    # if np.any(np.abs(hist - hist_cv2[0]) > 0.0000001):
-                    #    pu.db
-
                 else:
                     if descs is None or len(descs) == 0:
                         no_feat_counter += 1
@@ -173,7 +134,7 @@ def analyze_images(detector_name, descriptor_name, n_features, sensitivity, bow_
 
                     hist = compute_hamming_hist(descs, vocabulary)
 
-                if np.sum(hist) == 0:  # this shouldn't happen... histogram always sums to 1
+                if np.sum(hist) == 0:  # this shouldn't happen... histogram always sums to 1 (except when no match)
                     print "[Error] hist == 0 no kps for " + str(filename) + "--- \n"
                     continue
 
@@ -202,22 +163,19 @@ def analyze_images(detector_name, descriptor_name, n_features, sensitivity, bow_
         labels = np.load("cache/labels" + arg_string + ".npy")
         features_TSNE = np.load("cache/tsne" + arg_string + ".npy")
 
-    # subset_features = features_TSNE[(features_TSNE[:, 0] < -30) & (features_TSNE[:, 1] > 0)]
-    subset_features = features_TSNE
-
+    # create a border around images for labelling in scatter plot
     for i, image in enumerate(images):
-        image = min_resize(image, 40)
+        image = min_resize(image, img_size - border * 2)
         image = cv2.copyMakeBorder(image, 1, 1, 1, 1, cv2.BORDER_CONSTANT, value=(255,255,255))
-        if i == len(labels):
-            pu.db
 
+        b = border - 1
         if labels[i] == 180:
-            images[i] = cv2.copyMakeBorder(image, 4, 4, 4, 4, cv2.BORDER_CONSTANT, value=(17,141,246))
+            images[i] = cv2.copyMakeBorder(image, b, b, b, b, cv2.BORDER_CONSTANT, value=(17,141,246))
         else:
-            images[i] = cv2.copyMakeBorder(image, 4, 4, 4, 4, cv2.BORDER_CONSTANT, value=(245,219,10))
+            images[i] = cv2.copyMakeBorder(image, b, b, b, b, cv2.BORDER_CONSTANT, value=(245,219,10))
 
     # make plots
-    img = image_scatter(subset_features, images[0:len(subset_features)], scatter_size=8000)
+    img = image_scatter(features_TSNE, images, scatter_size=8000, img_size=img_size)
     cv2.imwrite("results/image_scatter" + arg_string + "n_feat " + str(len(features)) + ".png", img)
 
     plt.clf()
@@ -238,4 +196,24 @@ if __name__ == '__main__':
         n_features = int(sys.argv[3])
         sensitivity = int(sys.argv[4])
         bow_size = int(sys.argv[5])
-        analyze_images(detector_name, descriptor_name, n_features, sensitivity, bow_size, testing=False, cache=False)
+
+        warts = []
+
+        for root, dirnames, filenames in os.walk("classified/warts"):
+            for filename in fnmatch.filter(filenames, '*.png'):
+                warts.append("classified/warts" + "/" + filename)
+
+        negatives = []
+
+        for root, dirnames, filenames in os.walk("classified/negatives"):
+            for filename in fnmatch.filter(filenames, '*.png'):
+                negatives.append("classified/negatives" + "/" + filename)
+
+        warts_cream = []
+
+        for root, dirnames, filenames in os.walk("classified/warts_cream"):
+            for filename in fnmatch.filter(filenames, '*.png'):
+                warts_cream.append("classified/warts_cream" + "/" + filename)
+                warts.append("classified/warts_cream" + "/" + filename)
+
+        analyze_images(warts, negatives, detector_name, descriptor_name, n_features, sensitivity, bow_size, testing=False, cache=False, scatter_size=10000)
