f1619ea2d82357754200ee934d6947d196d06216
Hans Pinckaers <hans.pinckaers@gmail.com>
2016-09-23 13:01:07 +0200

    fix train

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ import numpy as np
 import time
 import random
 
-from pudb import set_trace
+# from pudb import set_trace
 from kmajority import kmajority, compute_hamming_hist
 from features import get_features_array
 from divide import divide_in
@@ -28,6 +28,7 @@ def cross_validate_with_participants(kfold, participants, detector_name='SIFT', 
 
     if save:
         # train a whole model
+        print "----- Training a whole model to save -----"
         model, vocabulary = train_model(filenames_pos, filenames_neg, detector_name, descriptor_name, dect_params, n_features, bow_size, k, model_params=model_params, classifier=classifier)
         joblib.dump(model, 'model.pkl')
         return 0.
@@ -312,4 +313,4 @@ if __name__ == '__main__':
         "gamma": 10.**params['svm_gamma']
     }
 
-    kappa = cross_validate_with_participants(5, parts, dect_params=dect_params, bow_size=params['bow_size'], model_params=model_params, classifier="forest")
+    kappa = cross_validate_with_participants(5, parts, dect_params=dect_params, bow_size=params['bow_size'], model_params=model_params, classifier="forest", save=True)
