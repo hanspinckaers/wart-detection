12b57a6c70a61dfb8f15816dfe7f80d4e9e35833
Hans Pinckaers <hans.pinckaers@gmail.com>
2016-09-26 15:05:36 +0200

    filter output for sigma

---
+++ bay_opt/filter_output.py
@@ -0,0 +1,21 @@
+import os
+import fnmatch
+import re
+
+sigma_re = re.compile("u")
+
+for root, dirnames, filenames in os.walk("output/"):
+    for filename in fnmatch.filter(filenames, '*.out'):
+        with open("output/" + filename, "r") as outputfile:
+            data = outputfile.readlines()
+            p = re.compile('[0-9.-]+')
+            sigma = p.findall(data[4])[5]
+            if float(sigma) < 1.:
+                print data[4]
+                print filename
+
+            # print str(data[4])
+            # m = sigma_re.match( 
+            #    if m:
+            #    print 'Match found: ', m.group()
+                # print 'No match'
